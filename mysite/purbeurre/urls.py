23cc9ef161a21f46dfcd76323167c86878249c33
megalex97 <megalex97@hotmail.com>
2019-09-16 14:10:38 +0300

    migration done, header and footer now in base.html

--- mysite/purbeurre/urls.py
+++ mysite/purbeurre/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'purbeurre'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('testpage/', views.test_page, name='testpage'),
+    path('login/', auth_views.LoginView, name='login'),
 ]
