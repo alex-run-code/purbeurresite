4bee76cfdf1b63d78bb2e7c3e207fb7cbb878927
unknown <megalex97@hotmail.com>
2019-10-23 22:31:43 +0300

    addition of form tests wrong mail, no name and wrond password

--- mysite/purbeurre/tests.py
+++ mysite/purbeurre/tests.py
@@ -22,27 +22,7 @@ from django.test import TestCase
 from .models import CustomUserManager, CustomUser
 from .forms import ItemForm, CustomUserCreationForm
 from django.urls import reverse
-
-class UserCreationTests(TestCase):
-
-    def test_user_was_created_with_correct_infos(self):
-        """ 
-        after the user fills the user creation form correctly,
-        he exists in the database with the right email and first name.
-        """
-        user = CustomUser(email='francois@gmail.com',first_name='francois',password='147www258Ab')
-        user.save()
-        self.assertIs('francois@gmail.com' in CustomUser.objects.all().values_list('email', flat=True),True)
-        self.assertIs(user.email is 'francois@gmail.com',True)
-        self.assertIs(user.first_name is 'francois',True)
-        
-
-# class ItemFormTest(TestCase):
-
-#     def test_form_renders_item_text_input(self):
-#         form = ItemForm()
-#         self.fail(form.as_p())
-
+from django.contrib.auth.forms import AuthenticationForm
 
 class CustomUserCreationFormTest(TestCase):
 
@@ -73,5 +53,22 @@ class CustomUserCreationFormTest(TestCase):
     def test_user_is_created_after_submitting_form(self):
         self.client.post(reverse('purbeurre:signup'), {'email':'francois@gmail.com', 'first_name':'francois', 'password1':'147www258Ab', 'password2':'147www258Ab'})
         self.assertEqual(CustomUser.objects.filter(email='francois@gmail.com').count(), 1)
+
+    def test_user_is_not_created_after_submitting_wrong_mail(self):
+        self.client.post(reverse('purbeurre:signup'), {'email':'francoisgmail.com', 'first_name':'francois', 'password1':'147www258Ab', 'password2':'147www258Ab'})
+        self.assertEqual(CustomUser.objects.filter(email='francoisgmail.com').count(), 0)
+
+    def test_user_is_not_created_after_submitting_no_name(self):
+        self.client.post(reverse('purbeurre:signup'), {'email':'francois@gmail.com', 'first_name':'', 'password1':'147www258Ab', 'password2':'147www258Ab'})
+        self.assertEqual(CustomUser.objects.filter(email='francois@gmail.com').count(), 0)
+
+    def test_user_is_not_created_after_submitting_wrong_password2(self):
+        self.client.post(reverse('purbeurre:signup'), {'email':'francois@gmail.com', 'first_name':'francois', 'password1':'147www258Ab', 'password2':'147ww258Ab'})
+        self.assertEqual(CustomUser.objects.filter(email='francois@gmail.com').count(), 0)
+
+    # trying to test that user is logged, but it doesnt work
+    def test_user_is_logged_in_after_logging_in(self):
+        self.client.post(reverse('login'), {'username':'cambefort.alex@gmail.com', 'password':'210991Ab'})
+        self.assertEqual(user.is_authenticated(), True)
 
 
