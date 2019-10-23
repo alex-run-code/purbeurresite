3ec7bf1b978acd08f01a12865015a6799f83b7cf
unknown <megalex97@hotmail.com>
2019-10-23 22:36:51 +0300

    addition of research function using requests

--- mysite/fooddb/dbfiller.py
+++ mysite/fooddb/dbfiller.py
@@ -1,42 +1,32 @@
-import openfoodfacts
+import requests
 
-categories = openfoodfacts.facets.get_categories({
-    'country':'france'
-})
+# research using request
+search_result = requests.get(
+    'https://world.openfoodfacts.org',
+    params={
+        'categories':'snacks',
+        'country':'france',
+        "page_size":"20",
+        'page':'3',
+        'json':'1',
+        },
+)
 
-for category in categories:
-    print(category['name'])
-
-
-search_result = openfoodfacts.products.advanced_search({
-  'search_terms':'Nutella',
-  'country':'france',
-  'language'
-  "sort_by":"product_name",
-  "page_size":"10",
-  'pages':'1',
-})
-
-print(search_result)
-print(search_result[1])
-print(search_result['products'][1])
-print(search_result['products'][1]['product_name'])
-print(search_result['products'][1]['categories'])
-print(search_result['products'][2]['categories'])
-print(search_result['products'][3]['categories'])
-print(search_result['products'][4]['categories'])
-
-
-for item in search_result:
-    print(item)
-
-for x in search_result['products']:
-    print(x)
-
-for x in search_result['products']:
-    print(x['product_name'])
-
+print(search_result.json()['products'][0])                                          # all info products
+print(search_result.json()['products'][0]['product_name'])                          # product name
+print(search_result.json()['products'][0]['image_front_url'])                       # picture
+print(search_result.json()['products'][0]['nutriments']['nutrition-score-fr'])      # nutriscore
+print(search_result.json()['products'][0]['nutriments']['sugars_100g'])             # sugar level for 100g
+print(search_result.json()['products'][0]['nutriments']['salt_100g'])               # salt level for 100g
+print(search_result.json()['products'][0]['nutriments']['carbohydrates_100g'])      # carbohydrates level for 100g
+print(search_result.json()['products'][0]['nutriments']['sodium_100g'])             # sodium level for 100g
+print(search_result.json()['products'][0]['nutriments']['saturated-fat_100g'])      # saturated level for 100g
+print(search_result.json()['products'][0]['nutriments']['proteins_100g'])           # proteins level for 100g
+print(search_result.json()['products'][0]['nutriments']['fat_100g'])                # fat level for 100g
 
 # add a food
 from fooddb.models import Food
 f = Food(name='Caramel', nutriscore=3)
+
+for item in search_result.json()['products']:
+    print(item['product_name'])
