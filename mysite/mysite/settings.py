8741c0f4f00691c0372bb0d8d6dad8f8a9ee14d3
unknown <megalex97@hotmail.com>
2019-11-12 17:07:11 +0200

    modification of settings.py - added mysite. in front of purbeurre and fooddb

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'purbeurre',
-    'fooddb',
+    'mysite.purbeurre',
+    'mysite.fooddb',
 ]
 
 MIDDLEWARE = [
