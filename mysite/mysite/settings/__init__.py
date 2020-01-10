b22357d6e6fd13e4f0ced36cec00c53844696b46
unknown <megalex97@hotmail.com>
2020-01-10 11:43:56 +0200

    switch psql to sqlite3 as default database

--- mysite/mysite/settings/__init__.py
+++ mysite/mysite/settings/__init__.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'purbeurredb',
-        'USER': 'alexandre',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
